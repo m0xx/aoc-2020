e112e9fe10b57a7e2e0d62727c3d10015182e8e4
Maxime Morin <maxime.morin@cortexmedia.ca>
2020-12-01 23:15:56 -0500

    day01 puzzle 2

--- days/01/index.js
+++ days/01/index.js
@@ -1,20 +1,15 @@
 const R = require('rambda');
+const {permutations} = require('../../lib/utils');
 const {readAndSplit} = require('../../lib/files');
+const sum = (items) => items.reduce((total, current) => total + current, 0);
 
-const listPossibilities = expenses => R.range(0, expenses.length - 1)
-    .reduce((results, i) => [
-        ...results,
-        ...R.range(i + 1, expenses.length).map(j => [expenses[i], expenses[j]])
-], []);
-const findPair = total => R.find(([a, b]) => a + b === total);
-
-
-const result = R.compose(
-    ([a, b]) => a * b,
-    findPair(2020),
-    listPossibilities,
+const resolveProblem = (filename, nbPermutation, total) => R.compose(
+    (items) => items.reduce((prev, current ) => prev * current, 1),
+    R.find((items) => sum(items) === total),
+    permutations(nbPermutation),
     R.map(expense => parseInt(expense, 10)),
     readAndSplit
-)('day-01.puzzle.txt');
+)(filename)
 
-console.log(result);
+
+console.log(resolveProblem('day-01.puzzle.txt', 3, 2020))
