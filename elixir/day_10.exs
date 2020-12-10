2789791b6f67f1eba98545cf14779ea78b08f144
Maxime Morin <maxime.morin2@gmail.com>
2020-12-10 18:25:16 -0500

    day08 p2

--- elixir/day_10.exs
+++ elixir/day_10.exs
@@ -1,10 +1,9 @@
 defmodule AOC2020Day10 do
   def parse() do
-    File.read!("inputs/day-10.sample.txt")
+    File.read!("inputs/day-10.puzzle.txt")
       |> String.split("\n", trim: true)
       |> Enum.map(&String.to_integer/1)
       |> Enum.sort
-#      |> (fn adapters -> [0] ++ adapters end).()
   end
 
   def find_adapter(prev, adapters, rating) do
@@ -33,28 +32,49 @@ defmodule AOC2020Day10 do
   end
 
   def list_possibilities(possibilities_by_adapter, adapters, acc) do
-    current = List.last(adapters[0])
+    current = List.first(adapters)
     next_adapters = Map.get(possibilities_by_adapter, current)
 
+#    IO.inspect({current, adapters, next_adapters, acc})
     cond  do
       next_adapters && length(next_adapters) > 0 ->
-        Enum.map(fn adapter ->
-          list_possibilities(
-                            possibilities_by_adapter,
-                            List.filter(fn a -> a > adapter end),
-                            acc ++ [current])
-        end)
+      Enum.reduce(next_adapters, [], fn
+        adapter, r_acc ->
+#      IO.inspect({:next, adapter})
+          filtered = Enum.filter(adapters, fn a -> a > adapter end)
+#          IO.inspect({adapter, filtered})
+          combinaisons = list_possibilities(possibilities_by_adapter, [adapter] ++ filtered, acc ++ [current])
+          r_acc ++ Enum.map(combinaisons, fn rest -> rest end)
+      end)
       true -> [acc ++ adapters]
     end
   end
+
+  def traverse(possibilities_by_adapter, current, acc) do
+    next_adapters = Map.get(possibilities_by_adapter, current)
+    cond  do
+      next_adapters && length(next_adapters) > 0 -> acc + Enum.reduce(next_adapters, 0, fn adapter, r_acc -> r_acc + traverse(possibilities_by_adapter, adapter, 0)  end)
+      true -> 1
+    end
+  end
+
   def part2() do
     adapters = parse()
+    IO.inspect(adapters)
+    adapters = List.insert_at(adapters, 0, 0)
+    adapters = adapters ++ [List.last(adapters) + 3]
+
     possibilities_by_adapter = Enum.reduce(adapters, %{}, fn adapter, acc ->
       Map.put(acc, adapter, Enum.filter(adapters, fn a -> a > adapter && a - adapter <= 3  end))
     end)
+    IO.inspect(possibilities_by_adapter)
 
-    adapters
-      |> list_possibilities(possibilities_by_adapter, adapters, [])
+    idx = Enum.find_index(adapters, fn a -> a == 77  end) |> IO.inspect
+
+    length(Map.keys(possibilities_by_adapter)) |>IO.inspect
+    a = traverse(Map.take(possibilities_by_adapter, Map.keys(possibilities_by_adapter) |> Enum.filter(fn n -> n < 77 end)), 0, 0)
+    b = traverse(Map.take(possibilities_by_adapter, Map.keys(possibilities_by_adapter) |> Enum.filter(fn n -> n >= 77 end)), 77, 0)
+    a * b
   end
 end
 
